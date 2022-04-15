3240b8599af6f82410c7252414bc2b4085378aaa
Jaeyeop-Jung <jjy5349@naver.com>
2022-04-15 19:07:03 +0900

    test: 하이퍼레저 패브릭 게이트웨이로 통신 & 기본 체인코드 테스트

---
+++ src/main/java/com/capstone/hyperledgerfabrictransferserver/api/TestApiController.java
@@ -0,0 +1,40 @@
+package com.capstone.hyperledgerfabrictransferserver.api;
+
+import com.capstone.hyperledgerfabrictransferserver.service.TestService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequiredArgsConstructor
+public class TestApiController {
+
+    private final TestService testService;
+
+    @GetMapping("/initledger")
+    public String init() throws Exception {
+        return testService.initLedger();
+    }
+
+    @GetMapping("/asset")
+    public String getAllAsset() throws Exception {
+        return testService.getAllAsset();
+    }
+
+    @PostMapping("/asset")
+    public String createAsset() throws Exception {
+        return testService.createAsset();
+    }
+
+    @PostMapping("/coin")
+    public String createCoin(
+            @RequestParam String assetId,
+            @RequestParam String coinName,
+            @RequestParam String coinValue
+    ) throws Exception {
+        return testService.createCoin(assetId, coinName, coinValue);
+    }
+
+}
---
+++ src/main/java/com/capstone/hyperledgerfabrictransferserver/service/TestService.java
@@ -0,0 +1,88 @@
+package com.capstone.hyperledgerfabrictransferserver.service;
+
+import lombok.RequiredArgsConstructor;
+import org.hyperledger.fabric.gateway.*;
+import org.springframework.stereotype.Service;
+
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
+@Service
+@RequiredArgsConstructor
+public class TestService {
+
+    static {
+        System.setProperty("org.hyperledger.fabric.sdk.service_discovery.as_localhost", "true");
+    }
+
+    public static Gateway connect() throws Exception{
+        // Load a file system based wallet for managing identities.
+        Path walletPath = Paths.get("wallet");
+        Wallet wallet = Wallets.newFileSystemWallet(walletPath);
+        // load a CCP
+//        Path networkConfigPath = Paths.get("","Users", "jeongjaeyeob", "go", "src", "github.com", "Jaeyeop-Jung", "fabric-samples", "test-network", "organizations", "peerOrganizations", "org1.example.com", "connection-org1.yaml");
+        Path networkConfigPath = Paths.get("/Users/jeongjaeyeob/go/src/github.com/Jaeyeop-Jung/fabric-samples/test-network/organizations/peerOrganizations/org1.example.com/connection-org1.yaml");
+
+        Gateway.Builder builder = Gateway.createBuilder();
+        builder.identity(wallet, "appUser").networkConfig(networkConfigPath).discovery(true);
+        return builder.connect();
+    }
+
+    public String initLedger() throws Exception {
+        Gateway gateway = connect();
+
+        Network network = gateway.getNetwork("mychannel");
+        Contract contract = network.getContract("basic");
+
+        byte[] result;
+
+        System.out.println("Submit Transaction: InitLedger creates the initial set of assets on the ledger.");
+        contract.submitTransaction("InitLedger");
+
+        return "OK";
+    }
+
+    public String getAllAsset() throws Exception {
+        Gateway gateway = connect();
+
+        Network network = gateway.getNetwork("mychannel");
+        Contract contract = network.getContract("basic");
+
+        byte[] result;
+
+        result = contract.evaluateTransaction("GetAllAssets");
+
+        return new String(result, StandardCharsets.UTF_8);
+    }
+
+    public String createAsset() throws Exception {
+        Gateway gateway = connect();
+
+        Network network = gateway.getNetwork("mychannel");
+        Contract contract = network.getContract("basic");
+
+        byte[] result;
+
+        contract.submitTransaction("CreateAsset", "asset13", "yellow", "5", "Tom", "1300");
+
+        return "OK";
+    }
+
+    public String createCoin(String assetId, String coinName, String coinValue) throws Exception {
+
+        Gateway gateway = connect();
+
+        Network network = gateway.getNetwork("mychannel");
+        Contract contract = network.getContract("basic");
+
+        byte[] result;
+
+        result = contract.submitTransaction("CreateCoin", assetId, coinName, coinValue);
+
+
+
+        return new String(result, StandardCharsets.UTF_8);
+    }
+
+}
