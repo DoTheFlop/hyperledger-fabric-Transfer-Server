b0ed8075941bc068b5d6dea9989034ae43e9114e
Jaeyeop-Jung <jjy5349@naver.com>
2022-05-03 10:53:22 +0900

    test: 스웨거 적용 테스트

--- src/main/java/com/capstone/hyperledgerfabrictransferserver/api/TestApiController.java
+++ src/main/java/com/capstone/hyperledgerfabrictransferserver/api/TestApiController.java
@@ -1,6 +1,7 @@
 package com.capstone.hyperledgerfabrictransferserver.api;
 
 import com.capstone.hyperledgerfabrictransferserver.service.TestService;
+import io.swagger.annotations.ApiOperation;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
